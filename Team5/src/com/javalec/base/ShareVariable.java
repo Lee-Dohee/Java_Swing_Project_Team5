03a3aaeee8dcf3b77ed7cb6ebfe52a9ed9daf8ae
Lee-Dohee <ehgml991210@naver.com>
2021-04-29 17:08:11 +0900

    삭제

--- Team5/src/com/javalec/base/ShareVariable.java
+++
@@ -1,9 +0,0 @@
-package com.javalec.base;
-
-public class ShareVariable {
-
-	public static final String url_mysql = "jdbc:mysql://localhost:3306/Test2?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
-    public static final String id_mysql = "root";
-    public static final String pw_mysql = "qwer1234";
-    
-}
