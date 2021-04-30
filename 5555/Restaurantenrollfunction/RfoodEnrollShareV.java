e9025776acc9f7a10e5d864d2a215dbbfcaebd4f
hyeonjin lim <cocolim2017@naver.com>
2021-04-30 20:19:08 +0900

    다시 올려요

---
+++ 5555/Restaurantenrollfunction/RfoodEnrollShareV.java
@@ -0,0 +1,9 @@
+package com.javalec.Restaurantenrollfunction;
+
+public class RfoodEnrollShareV {
+	
+	 public static final String url_mysql = "jdbc:mysql://127.0.0.1/Team5?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	 public static final String id_mysql = "root";
+	 public static final String pw_mysql = "qwer1234";
+
+}
